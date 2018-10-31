e8775226a924318d1eed64fbae8bf63ee24445a9
bwnnwb <44649006+bwnnwb@users.noreply.github.com>
2018-10-31 17:03:42 +0000

    Add files via upload

---
+++ google1.py
@@ -0,0 +1,6 @@
+for x in range(10):
+    print("GCI is great")
+name=input("Enter your name:")
+print("Hello " +name+", please to meet you!")
+namebackwards=name[::-1]
+print("Hello "+name+", please to meet you! Did you know that your name backwards is "+namebackwards+"?")
